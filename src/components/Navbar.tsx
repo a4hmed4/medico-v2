ec93c1d520bc3bd9cc66e116b040e588bfec96c6
sneed <100238373+a4hmed4@users.noreply.github.com>
2025-06-13 18:18:49 +0300

    Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -11,8 +11,8 @@ const Navbar: React.FC<NavbarProps> = ({ children }) => {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-16">
           <div className="flex items-center">
-            <img src="../../../Img/icon.ico" alt="kMedico" className="h-8 w-8" />
-            <span className="ml-2 text-xl font-bold text-gray-900 dark:text-white">Medvico</span>
+            <img src="../../Img/icon.ico" alt="Medico" className="h-8 w-8" />
+            <span className="ml-2 text-xl font-bold text-gray-900 dark:text-white">Medico</span>
           </div>
           <div className="hidden md:flex items-center space-x-8">
             <a href="#features" className="text-gray-600 dark:text-gray-300 hover:text-blue-600 dark:hover:text-blue-400">Features</a>
